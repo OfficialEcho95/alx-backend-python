a73c521769c4576d63624c8abf067f668ca89b11
Emmanuel Chukwu <officialecho95@outlook.com>
2023-07-06 23:10:13 +0100

    update done

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """
-a type-annotated function add that takes a float a and a 
+a type-annotated function add that takes a float a and a
 float b as arguments and returns their sum as a float.
 """
 
-def add(a:float, b:float) -> float:
+
+def add(a: float, b: float) -> float:
     """function that takes float and returns a float"""
-    return a + b;
+    return a + b
