7d425cae49a686e28f5314413a58a71ff4349ff9
Emmanuel Chukwu <officialecho95@outlook.com>
2023-07-07 00:10:59 +0100

    mixed variables

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+a module of mixed function
+"""
+
+
+def sum_mixed_list(mxd_lst: list[float | int]) -> float:
+    """takes int and dfloat and returns float"""
+    return sum(mxd_lst)
