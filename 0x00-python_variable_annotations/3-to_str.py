54ead7e135fcc34a4fad577c9e6c1ec3178122d5
Emmanuel Chukwu <officialecho95@outlook.com>
2023-07-06 23:55:49 +0100

    to string

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -7,8 +7,4 @@
 
 def to_str(n: float) -> str:
     """this function takes a float and returns it's string rep"""
-<<<<<<< HEAD
-    return "{}".format(n)
-=======
     return str(n)
->>>>>>> c9b32f964edebd27d8f72bfcea999cc81c1b6b8c
