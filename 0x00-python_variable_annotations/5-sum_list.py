d235e0758418c74cd3b69381117f169a5174a08b
Emmanuel Chukwu <officialecho95@outlook.com>
2023-07-07 00:14:33 +0100

    sum of lists

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -6,4 +6,7 @@ a module which contains a function as described by the function
 
 def sum_list(input_list: list[float]) -> float:
     """takes a list of floats and returns their sum"""
-    return sum(input_list)
+    result = 0.0
+    for num in input_list:
+        result += num
+    return result
