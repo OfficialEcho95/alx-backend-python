b32ad2d2c92b18d3289b4bfb970ef2736ccbde3c
Emmanuel Chukwu <officialecho95@outlook.com>
2023-07-07 00:32:43 +0100

    ints and floats

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,19 +1,14 @@
 #!/usr/bin/env python3
-"""Use mypy to validate the piece of code and apply any changes."""
-from typing import Tuple, List
+"""parameters and the return values, add type annotations to the function"""
+from typing import Mapping, Any, Union, TypeVar, Optional
+
+T = TypeVar('T')
 
 
-def zoom_array(lst: Tuple, factor: int = 2) -> List:
-    """Returns a list of integers multiplied by certain factor."""
-    zoomed_in: List = [
-        item for item in lst
-        for i in range(factor)
-    ]
-    return zoomed_in
-
-
-array: Tuple = (12, 72, 91)
-
-zoom_2x = zoom_array(array)
-
-zoom_3x = zoom_array(array, 3)
+def safely_get_value(dct: Mapping, key: Any, default:
+                     Optional[T] = None) -> Union[Any, T]:
+    """return dct"""
+    if key in dct:
+        return dct[key]
+    else:
+        return default
