f7526d658c575077a8770370478eff321c526e5a
Emmanuel Chukwu <officialecho95@outlook.com>
2023-07-07 01:04:36 +0100

    returning tuples

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+from typing import Union, Tuple
+"""
+a module containing the function described below
+"""
+
+
+def to_kv(k: str, v: Union[int , float]) -> Tuple[str, float]:
+    """a function that returns a tuple of str and float"""
+    return k, float(v * v)
