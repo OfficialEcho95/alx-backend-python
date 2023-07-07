27c2dcdea3d747bb0d7b44510091414270783a06
Emmanuel Chukwu <officialecho95@outlook.com>
2023-07-07 01:17:46 +0100

    corrections here and there

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -5,6 +5,6 @@ a module containing the function described below
 """
 
 
-def to_kv(k: str, v: Union[int , float]) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """a function that returns a tuple of str and float"""
     return k, float(v * v)
