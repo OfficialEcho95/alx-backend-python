3cecc48d26ba613bebbb50f82bfdd33843c65f4d
Emmanuel Chukwu <officialecho95@outlook.com>
2023-07-07 01:19:13 +0100

    annotate

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
-from typing import List, Tuple
-"""
-a module containing the special function
-"""
+"""unction’s parameters and return values with the appropriate types"""
+from typing import Iterable, Sequence, List, Tuple
 
 
-def element_length(lst: List[str]) -> List[Tuple[str, int]]:
-    """special decorations"""
+def element_length(lst: Iterable[Sequence]
+                   ) -> List[Tuple[Sequence, int]]:
+    """return i and lst"""
     return [(i, len(i)) for i in lst]
