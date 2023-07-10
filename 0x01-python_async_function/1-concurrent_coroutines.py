d11d11968a3c3274f7621d4e31224dab89f349ce
Emmanuel Chukwu <officialecho95@outlook.com>
2023-07-10 22:12:26 +0100

    import

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -12,17 +12,15 @@ async def wait_n(n: int, max_delay: float) -> list[float]:
     delays = []
     tasks = []
 
-    # Spawn wait_random n times
     for _ in range(n):
         task = asyncio.create_task(wait_random(max_delay))
         tasks.append(task)
 
-    # Await all the tasks and collect the delays
     for task in tasks:
         delay = await task
         heapq.heappush(delays, delay)
 
-    # Extract the minimum elements from the heap
+
     sorted_delays = []
     while delays:
         sorted_delays.append(heapq.heappop(delays))
