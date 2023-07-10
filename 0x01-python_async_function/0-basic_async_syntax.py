0bf5a733a6b4b3c1790f2733f8ae24f0c6932670
Emmanuel Chukwu <officialecho95@outlook.com>
2023-07-10 20:39:24 +0100

    random time

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -3,6 +3,7 @@
 A module that contains a funtion that takes a random
 float, sleeps then return the float
 """
+
 import asyncio
 import random
 
@@ -10,6 +11,5 @@ import random
 async def wait_random(max_delay: float = 10) -> float:
     """the function that receives the integer and passes it"""
     r = random.uniform(0, max_delay)
-
     await asyncio.sleep(r)
     return r
