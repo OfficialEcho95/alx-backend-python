616267f5ece473941ce306d1be43efb8741fc5c7
Emmanuel Chukwu <officialecho95@outlook.com>
2023-07-10 22:28:42 +0100

    more tasks

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,28 +1,17 @@
 #!/usr/bin/env python3
-"""
-a module that imports task 0
-"""
+"""execute multiple coroutines at the same time with async"""
+
 import asyncio
-import heapq
-from 0-basic_async_syntax.py import wait_random
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: float) -> list[float]:
-    """a function that takes 2 int variables"""
-    delays = []
-    tasks = []
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    2 int arguments (in this order): n and max_delay
+    async routine called wait_n
+    """
 
-    for _ in range(n):
-        task = asyncio.create_task(wait_random(max_delay))
-        tasks.append(task)
-
-    for task in tasks:
-        delay = await task
-        heapq.heappush(delays, delay)
-
-
-    sorted_delays = []
-    while delays:
-        sorted_delays.append(heapq.heappop(delays))
-
-    return sorted_delays
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    return [await task for task in asyncio.as_completed(tasks)]
