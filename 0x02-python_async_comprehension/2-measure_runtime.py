253672e02855516aaf9c40cc4680d1732adc116b
Emmanuel Chukwu <officialecho95@outlook.com>
2023-07-11 22:06:57 +0100

    generator

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Run time for four parallel comprehensions
+"""
+
+import asyncio
+from async_comprehension import async_comprehension
+
+
+async def measure_runtime():
+    """coroutine that will execute async_comprehension four times"""
+    start_time = asyncio.get_event_loop().time()
+
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
+    )
+
+    end_time = asyncio.get_event_loop().time()
+    return end_time - start_time
