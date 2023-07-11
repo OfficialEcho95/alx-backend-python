501dbc3ee3d36ffe16b20980f931e3f2e5ca25c6
Emmanuel Chukwu <officialecho95@outlook.com>
2023-07-11 21:05:05 +0100

    basics

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -9,9 +9,6 @@ import random
 
 async def async_generator():
     """function which takes no argument and returns the generated random"""
-    i: int = 0
-    while (i < 10):
+    for _ in range(10):
         await asyncio.sleep(1)
-        r = random.uniform(0, 10)
-        yield r
-        i += 1
+        yield random.uniform(0, 10)
